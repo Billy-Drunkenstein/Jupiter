427c43d6c79e21393184bc6a3924b74f8d427dfa
Billy-Drunkenstein <ry2502@columbia.edu>
2026-02-02 14:23:40 -0500

    Update config.py

--- SigTest/config.py
+++ SigTest/config.py
@@ -1,1 +1,22 @@
+from pathlib import Path
 
+class ValidationConfig:
+
+    # === Paths ===
+    feature_path: Path
+    target_path: Path
+    meta_path: Path
+    calendar_path: Path 
+    output_path: Path
+
+    # === Backtest parameters ===
+    asof: int
+    lookback: int
+    lookforward: int
+    target_cols: list[str]
+
+    # === Method selection ===
+    methods: list[str]
+
+    # === Hyperparameters ===
+    hyperparams: dict
